176905c7e8806e1131424e2bc23b56d5bbd33127
Quang Pham <quangpn.developer@gmail.com>
2026-03-20 13:34:44 +0700

    Initial commit

---
+++ packages/ui/src/lib/utils.ts
@@ -0,0 +1,13 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
+
+export const focusRing = cn(
+  'focus-visible:outline-none focus-visible:ring-2',
+  'focus-visible:ring-ring focus-visible:ring-offset-2',
+);
+
+export const disabled = 'disabled:pointer-events-none disabled:opacity-50';
---
+++ packages/ui/vitest.config.ts
@@ -0,0 +1,14 @@
+import { reactConfig } from '@seaguntech/vitest-config/react';
+import { defineConfig, mergeConfig } from 'vitest/config';
+
+export default mergeConfig(
+  reactConfig,
+  defineConfig({
+    test: {
+      include: ['tests/**/*.{test,spec}.{ts,tsx}'],
+      coverage: {
+        all: false,
+      },
+    },
+  }),
+);
